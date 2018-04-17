b5fb4d0ce1c07b378665e4a1e3eda09edca4eed1
lelamhai <leelamhair@gmail.com>
2018-04-17 11:23:50 +0700

    create event

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour {
+    public delegate void ChangeColor(Color color);
+    public static event ChangeColor onChangeColor;
+
+	// Use this for initialization
+	void Start () {
+		if(onChangeColor != null)
+        {
+            onChangeColor(Color.red);
+        }
+	}
+	
+	
+}
---
+++ Assets/Scripts/SpriteControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MainGame.onChangeColor += FunctionChangeColor;
+	}
+
+    void FunctionChangeColor(Color color)
+    {
+        GetComponent<SpriteRenderer>().color = color;
+    }
+}
